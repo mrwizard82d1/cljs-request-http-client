8346a86d332f5fcb371c5e395550a8f8cf801b13
Larry Jones <larryj@blinds.com>
2017-05-20 13:11:23 -0500

    Explain my mental model of the code

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -3,20 +3,33 @@
 
 (nodejs/enable-util-print!)
 
+;; Keep a private reference to the `request` package
 (def ^:private js-request (nodejs/require "request"))
 
-(defn request [url-or-opts callback]
+
+(defn request 
+  "Request an internet resource.
+
+  `url-or-opts` Specifies the resource using a URL or a map allowing one to specify additional request 
+  details. For more details, see https://github.com/request/request#requestoptions-callback.
+  `callback` A function of three arguments to invoke when the request completes.
+      `error` Error typically when making a request
+      `response` The response for the request
+      `body` The body of the response."
+  [url-or-opts callback]
   (js-request (clj->js url-or-opts) callback))
 
+;; Simply to test the `request` function
 (defn test-request []
-  (request {:method "GET"
+  (request {;; Issue an HTTP GET for the resource identified by `:uri`
+            :method "GET"
             :uri "http://www.google.com"}
            (fn [error response body]
              (when (and (not error) (= 200 (.-statusCode response)))
                (println body)))))
 
 (defn main [& args]
-  (println "Abracadabra!")
+  (println "Abracadabra!") ; just for "debugging"
   (test-request))
 
 (set! *main-cli-fn* main)
