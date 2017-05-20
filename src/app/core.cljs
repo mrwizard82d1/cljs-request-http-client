f56f70000de6519722aea7526f3a27c3c963e534
Larry Jones <larryj@blinds.com>
2017-05-20 12:27:48 -0500

    Successfully request page from Google

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -5,8 +5,18 @@
 
 (def ^:private js-request (nodejs/require "request"))
 
+(defn request [url-or-opts callback]
+  (js-request (clj->js url-or-opts) callback))
+
+(defn test-request []
+  (request {:method "GET"
+            :uri "http://www.google.com"}
+           (fn [error response body]
+             (when (and (not error) (= 200 (.-statusCode response)))
+               (println body)))))
+
 (defn main [& args]
   (println "Abracadabra!")
-  (println js-request))
+  (test-request))
 
 (set! *main-cli-fn* main)
