9ea10937eee83bbf520071b9c8a43360b12b2829
Larry Jones <larryj@blinds.com>
2017-05-20 18:19:26 -0500

    Success - with channels!
    It was not quite as easy as creating success with callbacks; I have much
    to learn about `core.async. However, I like the resultant code structure
    much better. I do not have to read through all the callbacks to
    determine what the code does. :)

--- build.boot
+++ build.boot
@@ -1,6 +1,7 @@
 (set-env!
   :source-paths #{"src"}
-  :dependencies '[[adzerk/boot-cljs "2.0.0" :scope "test"]])
+  :dependencies '[[adzerk/boot-cljs "2.0.0" :scope "test"]
+                  [org.clojure/core.async "0.3.442"]])
 
 (require
   '[adzerk.boot-cljs :refer [cljs]])
--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,12 +1,40 @@
 (ns app.core
-  (:require [cljs.nodejs :as nodejs]))
+  (:require [cljs.nodejs :as nodejs]
+            [cljs.core.async :as async])
+  (:require-macros [cljs.core.async.macros :as async-m]))
 
 (nodejs/enable-util-print!)
 
 (def ^:private js-request (nodejs/require "request"))
 
+(defn request 
+  "Request an internet resource.
+
+  `url-or-opts` Specifies the resource using a URL or a map allowing one to specify additional request 
+  details. For more details, see https://github.com/request/request#requestoptions-callback.
+
+  This function returns a channel for asynchronous reads and writes. The (local) callback function will
+  write the response to this channel when available. The caller can read the response at her leisure."
+  [url-or-opts]
+  (let [channel (async/chan 1)]
+    (js-request (clj->js url-or-opts)
+                (fn [error response body]
+                  (async/put! channel 
+                              (if error 
+                                {:error error}
+                                {:response response
+                                 :body body})
+                              #(async/close! channel))))
+    channel))
+
 (defn main [& args]
   (println "Abracadabra!")
-  (println js-request))
+  (async-m/go 
+    (let [result (async/<! (request "http://httpbin.org/html"))]
+      (cond (:error result) (println (:error result))
+            :else (do
+                    (println (.-statusCode (:response result)))
+                    (println (:body result)))))))
+
 
 (set! *main-cli-fn* main)
